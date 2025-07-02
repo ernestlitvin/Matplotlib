b50a2d365f8af5ea5543526081d01a63267e260a
ernestlitvin <erik.litvin@gmail.com>
2025-07-02 13:23:19 +0300

    starting learning

---
+++ main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+# langas, grafikas = plt.subplots()
+# plt.show()
+
+# langas, grafikas = plt.subplots(2,2)
+# plt.show()
+
+x = [5,15,20,25]
+y = [14,26,5,44]
+mer,per = plt.subplots()
+per.plot(x,y)
+plt.show()
+
+
+
+
+
+
+
+
+
+
