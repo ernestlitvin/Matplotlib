e32f76e74d44abe834f99d82acb50bda5dc5b471
ernestlitvin <erik.litvin@gmail.com>
2025-07-02 13:57:06 +0300

    uzd 1

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 # langas, grafikas = plt.subplots()
 # plt.show()
@@ -6,12 +8,31 @@ import matplotlib.pyplot as plt
 # langas, grafikas = plt.subplots(2,2)
 # plt.show()
 
-x = [5,15,20,25]
-y = [14,26,5,44]
-mer,per = plt.subplots()
-per.plot(x,y)
-plt.show()
+# x = [5,15,20,25]
+# y = [14,26,5,44]
+# mer,per = plt.subplots()
+# per.plot(x,y,marker='8', linestyle=':', color='#c875c4', markersize=12)
+# plt.show()
 
+# def f(t):
+#     return np.exp(-t) * np.cos(2*np.pi*t)
+# t1 = np.arange(0.0, 5.0, 0.1)
+# fig, ax = plt.subplots()
+# ax.plot(t1, f(t1), color='red', marker='d', ms=12, lw=3.0, label='$e^{-x}\cdot\cos(2\pi x)$')
+# ax.set_ylabel('Y ašies pavadinimas')
+# ax.set_xlabel('X ašies pavadinimas')
+# ax.set_title('Grafiko pavadinimas')
+# ax.legend(loc=9) # 0 - 9
+# plt.show()
+
+###--- UZD 1 --- ###
+
+# A) Duotas sąrašas x=[1,2,3,4,5,6,7,8,9]
+# Viename lange, skirtinguose grafikuose atvaizduokite 𝑥2, 𝑥 ⋅ 3,
+# 𝑥 ⋅ 𝑎, čia a - įvedamas vartotojo.
+# Panaudokite skirtingas spalvas, linijų tipus.
+
+x=[1,2,3,4,5,6,7,8,9]
 
 
 
